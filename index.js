28c591efdd919c71e9c1d8f6ae0b3a1df7c7d0ea
shubham14600.ss@gmail.com <shubham14600.ss@gmail.com>
2020-09-27 16:29:13 +0530

    Add feature Scroll to Statistics section

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ $(document).ready(function() {
   }, 200); 
 }); 
 
+$("a").click(function(){
+  var pageId = $(this).attr("data-page");
+  $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
+});
 
 
 
