d8ee959d530e61565dc3278257938213acdf4604
flitzmare <riksa_leone@yahoo.co.id>
2020-10-01 10:14:00 +0700

    added countdown in the home page

--- index.js
+++ index.js
@@ -36,6 +36,39 @@ $(document).ready(function() {
   }, 200); 
 }); 
 
+//Countdown
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        deadline = "Oct 31, 2020 23:59:59";
+
+  let countDown = new Date(deadline).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let countdown = document.getElementById("countdown"),
+              finishmessage = document.getElementById("finishmessage");
+
+          countdown.style.display = "none";
+          finishmessage.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
 
 
 
