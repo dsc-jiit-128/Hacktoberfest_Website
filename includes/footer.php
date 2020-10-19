e45ef92d68e6287eb2e21f8be5b29c1753703ca4
Jai Mani Kunal <jaimanikunal@gmail.com>
2020-10-19 12:54:20 +0530

    Resolve Footer Issue and add footer to every pages also Added visible and clear github logo in the footer

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<?php
+        include 'includes/modal.php';
+        ?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-xs-6">
+            <div class="columnf">
+              <h3>Information</h3>
+                <p><a href="about_us.php"><span style="color:white">About Us</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">Contact Us</span></a></p>
+            </div>
+            </div>
+           <div class="col-md-4 col-xs-6">
+            <div class="columnf">
+              <h3>My Account</h3>
+                <p><a href="#" data-toggle="modal" data-target="#loginmodal" ><span style="color:white">Log In</span></a></p>
+                <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p>
+            </div>
+           </div>
+            <div class="col-md-4 col-md-6">
+            <div class="columnf">
+              <h3>Contact Us</h3>
+                <p>CONTACT No.(+91)8462526854</p>
+                <p>Copyright © Mobile's Store. All Rights Reserved </p>
+            </div>
+        </div>
+        </div>
+    </div>
+</footer>
